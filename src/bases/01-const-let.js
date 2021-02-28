88e250812a5a1a38a10a1d061caf195eee290ff0
Hector <hectoralarconguerra@gmail.com>
2021-02-27 20:35:33 -0500

    Basicos

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+
+//Variables y constantes
+
+const nombre = 'Hector';
+const apellido = 'Alarcon';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado)
+
+if(true){
+    const nombre = 'Alberto';
+    console.log(nombre)
+}
+
+console.log(valorDado);
